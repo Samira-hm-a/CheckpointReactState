d7a3e1a11454346075f45e5fc74ab9e7cf5fce73
Samira Hmani <samira.hmani90@gmail.com>
2020-10-08 14:35:47 +0100

    reactstate

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Person from './Components/Person';
+import Time from './Components/Time';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className='App'>
+        <h2> To See My Card Click Here </h2>
+        <Person/>
+        <Time/>
+      </div>
+
+    )
+  }
+}
+    export default App;
---
+++ src/Components/Person.js
@@ -0,0 +1,47 @@
+
+import React, { Component } from 'react';
+// import PropTypes from "prop-types";
+import { Card, ListGroup, ListGroupItem} from "react-bootstrap";
+import  './css/Main.css'
+import profilphoto from "./../Assets/profilphoto.jpg"
+
+class Person extends Component {
+   // constructor(props){
+   //super(props);
+   state = {
+       imgSrc: profilphoto,
+       fullName: "Samira HMANI",
+       bio: "I have a Master's degree in Auditing and Computer Security",
+       profession: "Student Full-Stack JavaScript in Go My Code",
+       show: false,
+    };
+   handleClick = () => {
+    this.setState({ show: !this.state.show });
+  };
+   
+   render() {
+       return(
+        <div className="Person">
+        <button type="button" class="btn btn-info"
+          onClick={this.handleClick}>
+            SHOW
+        </button>
+       {this.state.show ? (
+          <Card style={{ width: "20rem" , border:"6px"}}>
+            <Card.Img variant="top" src={this.state.imgSrc} />
+            <Card.Body>
+              <Card.Title>{this.state.fullName}</Card.Title>
+              <Card.Text>
+                  {this.state.bio}
+              </Card.Text> <br/> </Card.Body>
+             <ListGroup className="list-group-flush">
+                <ListGroupItem>{this.state.profession} </ListGroupItem>
+             </ListGroup>
+
+          </Card>
+        ) : null}
+    </div>
+    );}
+}
+
+export default Person
